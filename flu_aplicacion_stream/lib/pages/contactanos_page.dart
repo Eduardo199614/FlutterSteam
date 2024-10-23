e37fa643210354a8aee82cb7098ab5e93c7a9500
Pedro Valverde <77286190+Eduardo199614@users.noreply.github.com>
2024-10-23 07:54:28 -0500

    Cambios en login y  contactanos

--- flu_aplicacion_stream/lib/pages/contactanos_page.dart
+++ flu_aplicacion_stream/lib/pages/contactanos_page.dart
@@ -29,136 +29,140 @@ class _ContactanosPageState extends State<ContactanosPage> {
               borderRadius: BorderRadius.circular(12),
               border: Border.all(color: Colors.grey.shade400, width: 2),
             ),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Formulario de Contacto',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 16),
-
-                  // Nombre
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade50,
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.blue, width: 1),
+            child: SingleChildScrollView(
+              // Agregado para permitir desplazamiento
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Formulario de Contacto',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    padding: EdgeInsets.all(8),
-                    margin: EdgeInsets.only(bottom: 16),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Nombre',
-                        border: InputBorder.none,
+                    SizedBox(height: 16),
+
+                    // Nombre
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade50,
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.blue, width: 1),
                       ),
-                      onSaved: (value) => _nombre = value,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingrese su nombre';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-
-                  // Teléfono
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.green.shade50,
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.green, width: 1),
-                    ),
-                    padding: EdgeInsets.all(8),
-                    margin: EdgeInsets.only(bottom: 16),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Teléfono',
-                        border: InputBorder.none,
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.only(bottom: 16),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Nombre',
+                          border: InputBorder.none,
+                        ),
+                        onSaved: (value) => _nombre = value,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingrese su nombre';
+                          }
+                          return null;
+                        },
                       ),
-                      keyboardType: TextInputType.phone,
-                      onSaved: (value) => _telefono = value,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingrese su número de teléfono';
-                        }
-                        return null;
-                      },
                     ),
-                  ),
 
-                  // Correo
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.orange.shade50,
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.orange, width: 1),
-                    ),
-                    padding: EdgeInsets.all(8),
-                    margin: EdgeInsets.only(bottom: 16),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Correo Electrónico',
-                        border: InputBorder.none,
+                    // Teléfono
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.green.shade50,
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.green, width: 1),
                       ),
-                      keyboardType: TextInputType.emailAddress,
-                      onSaved: (value) => _correo = value,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingrese su correo electrónico';
-                        }
-                        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                          return 'Por favor ingrese un correo válido';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-
-                  // Mensaje
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.purple.shade50,
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.purple, width: 1),
-                    ),
-                    padding: EdgeInsets.all(8),
-                    margin: EdgeInsets.only(bottom: 16),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Mensaje',
-                        border: InputBorder.none,
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.only(bottom: 16),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Teléfono',
+                          border: InputBorder.none,
+                        ),
+                        keyboardType: TextInputType.phone,
+                        onSaved: (value) => _telefono = value,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingrese su número de teléfono';
+                          }
+                          return null;
+                        },
                       ),
-                      maxLines: 4,
-                      onSaved: (value) => _mensaje = value,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Por favor ingrese su mensaje';
-                        }
-                        return null;
-                      },
                     ),
-                  ),
 
-                  // Botón de enviar
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _formKey.currentState!.save();
-                          // Aquí puedes manejar el envío de los datos
-                          print('Nombre: $_nombre');
-                          print('Teléfono: $_telefono');
-                          print('Correo: $_correo');
-                          print('Mensaje: $_mensaje');
-                        }
-                      },
-                      child: Text('Enviar mensaje'),
+                    // Correo
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.orange.shade50,
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.orange, width: 1),
+                      ),
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.only(bottom: 16),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Correo Electrónico',
+                          border: InputBorder.none,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onSaved: (value) => _correo = value,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingrese su correo electrónico';
+                          }
+                          if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                            return 'Por favor ingrese un correo válido';
+                          }
+                          return null;
+                        },
+                      ),
                     ),
-                  ),
-                ],
+
+                    // Mensaje
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.purple.shade50,
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.purple, width: 1),
+                      ),
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.only(bottom: 16),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Mensaje',
+                          border: InputBorder.none,
+                        ),
+                        maxLines: 4,
+                        onSaved: (value) => _mensaje = value,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor ingrese su mensaje';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+
+                    // Botón de enviar
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                            // Aquí puedes manejar el envío de los datos
+                            print('Nombre: $_nombre');
+                            print('Teléfono: $_telefono');
+                            print('Correo: $_correo');
+                            print('Mensaje: $_mensaje');
+                          }
+                        },
+                        child: Text('Enviar mensaje'),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- flu_aplicacion_stream/lib/pages/login_page.dart
+++ flu_aplicacion_stream/lib/pages/login_page.dart
@@ -20,64 +20,98 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'Correo electrónico'),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingrese su correo';
-                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                    return 'Por favor, ingrese un correo válido';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Contraseña'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingrese su contraseña';
-                  } else if (value.length < 6) {
-                    return 'La contraseña debe tener al menos 6 caracteres';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _phoneController,
-                decoration: InputDecoration(labelText: 'Teléfono'),
-                keyboardType: TextInputType.phone,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingrese su teléfono';
-                  } else if (value.length != 10) {
-                    return 'Ingrese un número de teléfono válido';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // Si todo está validado correctamente, navega al HomePage
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
-                    );
-                  }
-                },
-                child: Text('Ingresar'),
-              ),
-            ],
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Iniciar Sesión',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          controller: _emailController,
+                          decoration:
+                              InputDecoration(labelText: 'Correo electrónico'),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, ingrese su correo';
+                            } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                .hasMatch(value)) {
+                              return 'Por favor, ingrese un correo válido';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(labelText: 'Contraseña'),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, ingrese su contraseña';
+                            } else if (value.length < 6) {
+                              return 'La contraseña debe tener al menos 6 caracteres';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          controller: _phoneController,
+                          decoration: InputDecoration(labelText: 'Teléfono'),
+                          keyboardType: TextInputType.phone,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, ingrese su teléfono';
+                            } else if (value.length != 10) {
+                              return 'Ingrese un número de teléfono válido';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              // Si todo está validado correctamente, navega al HomePage
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()),
+                              );
+                            }
+                          },
+                          child: Text('Ingresar'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
